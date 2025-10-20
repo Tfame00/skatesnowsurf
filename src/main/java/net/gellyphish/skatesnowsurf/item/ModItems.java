c77469f7017ec2f884c4add171cd736e4a302172
tyler <tlfosterfam@gmail.com>
2025-10-19 22:58:42 -0400

    surfboard :)

--- src/main/java/net/gellyphish/skatesnowsurf/item/ModItems.java
+++ src/main/java/net/gellyphish/skatesnowsurf/item/ModItems.java
@@ -12,6 +12,8 @@ public class ModItems {
     public static final Item WHEEL = registerItem("wheel", new Item(new Item.Settings()));
     public static final Item FIN = registerItem("fin", new Item(new Item.Settings()));
 
+    public static final Item SURFBOARD = registerItem("surfboard", new Item(new Item.Settings()));
+
 
     private static Item registerItem(String name, Item item) {
         return Registry.register(Registries.ITEM, Identifier.of(SkateSnowSurf.MOD_ID, name), item);
@@ -24,6 +26,10 @@ public class ModItems {
         ItemGroupEvents.modifyEntriesEvent(ItemGroups.INGREDIENTS).register(fabricItemGroupEntries -> {
             fabricItemGroupEntries.add(WHEEL);
             fabricItemGroupEntries.add(FIN);
+        });
+
+        ItemGroupEvents.modifyEntriesEvent(ItemGroups.TOOLS).register(fabricItemGroupEntries -> {
+            fabricItemGroupEntries.add(SURFBOARD);
         });
     }
 }
