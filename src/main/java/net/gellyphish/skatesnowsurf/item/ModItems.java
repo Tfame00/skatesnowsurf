9e10ce5477a4510ee5754b7874d24bfe67d67c92
tyler <tlfosterfam@gmail.com>
2025-10-20 22:09:46 -0400

    removed surfboard :( (it was ugly)
    added board tiers

--- src/main/java/net/gellyphish/skatesnowsurf/item/ModItems.java
+++ src/main/java/net/gellyphish/skatesnowsurf/item/ModItems.java
@@ -12,8 +12,12 @@ public class ModItems {
     public static final Item WHEEL = registerItem("wheel", new Item(new Item.Settings()));
     public static final Item FIN = registerItem("fin", new Item(new Item.Settings()));
 
-    public static final Item SURFBOARD = registerItem("surfboard", new Item(new Item.Settings()));
-
+    public static final Item WOODEN_BOARD = registerItem("wooden_board", new Item(new Item.Settings()));
+    public static final Item STONE_BOARD = registerItem("stone_board", new Item(new Item.Settings()));
+    public static final Item IRON_BOARD = registerItem("iron_board", new Item(new Item.Settings()));
+    public static final Item GOLDEN_BOARD = registerItem("golden_board", new Item(new Item.Settings()));
+    public static final Item DIAMOND_BOARD = registerItem("diamond_board", new Item(new Item.Settings()));
+    public static final Item NETHERITE_BOARD = registerItem("netherite_board", new Item(new Item.Settings()));
 
     private static Item registerItem(String name, Item item) {
         return Registry.register(Registries.ITEM, Identifier.of(SkateSnowSurf.MOD_ID, name), item);
@@ -26,10 +30,12 @@ public class ModItems {
         ItemGroupEvents.modifyEntriesEvent(ItemGroups.INGREDIENTS).register(fabricItemGroupEntries -> {
             fabricItemGroupEntries.add(WHEEL);
             fabricItemGroupEntries.add(FIN);
-        });
-
-        ItemGroupEvents.modifyEntriesEvent(ItemGroups.TOOLS).register(fabricItemGroupEntries -> {
-            fabricItemGroupEntries.add(SURFBOARD);
+            fabricItemGroupEntries.add(WOODEN_BOARD);
+            fabricItemGroupEntries.add(STONE_BOARD);
+            fabricItemGroupEntries.add(IRON_BOARD);
+            fabricItemGroupEntries.add(GOLDEN_BOARD);
+            fabricItemGroupEntries.add(DIAMOND_BOARD);
+            fabricItemGroupEntries.add(NETHERITE_BOARD);
         });
     }
 }
